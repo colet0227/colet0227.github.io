1105a5c79a32dfdea81c103524bb3531eeb09576
Cole Thompson <colet0227@gmail.com>
2023-05-22 08:09:04 -0700

    removed margins

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -8,18 +8,32 @@ const ThemeProvider = ({ children }) => {
 
   useEffect(() => {
     const darkMediaQuery = window.matchMedia('(prefers-color-scheme: light)');
-    setThemeName(darkMediaQuery.matches ? 'dark' : 'light')
+    const initialTheme = darkMediaQuery.matches ? 'dark' : 'light';
+    setThemeName(initialTheme);
+  
+    // Add initial theme class to body
+    document.body.classList.add(initialTheme);
+  
     darkMediaQuery.addEventListener('change', (e) => {
-      setThemeName(e.matches ? 'dark' : 'light')
+      const newTheme = e.matches ? 'dark' : 'light';
+      setThemeName(newTheme);
+  
+      // Remove old theme class from body and add new theme class
+      document.body.classList.remove('light', 'dark');
+      document.body.classList.add(newTheme);
     });
-  }, [])
-
+  }, []);
+  
   const toggleTheme = () => {
-    const name = themeName === 'dark' ? 'light' : 'dark'
-    localStorage.setItem('themeName', name)
-    setThemeName(name)
-  }
-
+    const newTheme = themeName === 'dark' ? 'light' : 'dark';
+    localStorage.setItem('themeName', newTheme);
+    setThemeName(newTheme);
+  
+    // Remove old theme class from body and add new theme class
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(newTheme);
+  };
+  
   return (
     <ThemeContext.Provider value={{ themeName, toggleTheme }}>
       {children}
